b2fb86d85637bc5d7963fe2faa8051e21d99ea1c
Markuss Balodis <98739084+vFar@users.noreply.github.com>
2022-06-14 15:11:08 +0000

    Izstrādāti vēl 2 teorijas jautājumi par DO-WHILE ciklu un izstrādāta potenciāla funkcija, kas testa beigās izdrukās nepareizo atbildēto jautājumu sarakstu

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unistd.h>
+#include <algorithm>
 using namespace std;
 int main() {
   string answer;
@@ -9,11 +10,12 @@ int main() {
   
   cout<<"DO-WHILE tests\n";
   cout<<"Tests satur 10 jautājumus, kurā katram jautājumam ir 4 atbilžu varianti!\n";
-
+  /*
   sleep(4);
+  */
   system("clear");
   
-    cout<<"Jautājumā ir 3 pareizas atbildes un atbildes         formāts: A;B;C, B;C;A u.c.!\n\n\n";
+    cout<<"Jautājumā ir 3 pareizas atbildes un atbildes formāts: A;B;C, B;C;A u.c.!\n\n\n";
     cout<<"1. Kurš nav cikls?\n\n\n";
     cout<<"\tA: SWITCH \t\t B: IF\n";
     cout<<"\tC: STRING \t\t D: FOR\n\n";
@@ -26,4 +28,39 @@ int main() {
     }
 
     system("clear");
+
+    cout<<"Jautājumā ir 2 pareizas atbildes un atbildes formāts: A;B;C, B;C;A u.c.!\n\n\n";
+    cout<<"1. Kurš apgalvojums ir patiess par DO-WHILE ciklu?\n\n\n";
+    cout<<"\tA: Cikls nostrādās vienu reizi \n\tB: Nenostrādās nevienu reizi\n";
+    cout<<"\tC: Lēns cikls\n\tD: Cikls ir exit-controlled loop\n\n";
+    cin>>answer;
+    
+    if(answer == "A;D" || answer == "D;A"){
+      points++;
+    }else{
+      wrong[1] = "2. Kurš apgalvojums ir patiess par DO-WHILE ciklu?";
+    }
+
+    system("clear");
+
+    cout<<"Jautājumā ir 2 pareizas atbildes un atbildes formāts: A;B;C, B;C;A u.c.!\n\n\n";
+    cout<<"3. Kuri apgalvojumi ir nepatiesi par ciklu?\n\n\n";
+    cout<<"\tA: DO-WHILE cikli ir pēcpārbaudes(posttest) cikli \n\tB: Cikls nevar būt bezgalīgs\n";
+    cout<<"\tC: Pastāv semikols pēc while(nosacījums) sintakses\n\tD: Nostrādās vismaz vienu reizi\n\n";
+    cin>>answer;
+    
+    if(answer == "B;C" || answer == "C;B"){
+      points++;
+    }else{
+      wrong[1] = "3. Kurš apgalvojums ir patiess par DO-WHILE ciklu?";
+    }
+
+  
+  
+  
+    sort(begin(wrong), end(wrong));
+    for(int i=0; i<10; i++){
+      cout<<wrong[i]<<"\n";
+    }
+    
 }
